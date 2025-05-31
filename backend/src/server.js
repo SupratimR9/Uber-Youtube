95ae3d2f0873d44b9192738d60d717980327a09b
SupratimR9 <supratimd047@gmail.com>
2025-05-31 17:33:54 +0530

    New Beginnings

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({
+  path: ".env",
+});
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`\nShutting down the server due to Uncaught Exception !!!`);
+  process.exit(1);
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running at port : ${process.env.PORT}`);
+});
